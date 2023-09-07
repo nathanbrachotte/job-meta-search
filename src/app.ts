817d201288229956ab7e549d70ae8f7c274c1207
N8 <nathan.brachotte+github@gmail.com>
2023-09-07 16:41:36 +0200

    feat: 🎸 add MySQL DB

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { router as authRouter } from '@/routes/auth.route'
 import { router as userRouter } from '@/routes/users.route'
 import validateEnv from '@/utils/validateEnv'
-import { CREDENTIALS, LOG_FORMAT, ORIGIN } from '@config'
+import { CREDENTIALS, DB_URL, LOG_FORMAT, ORIGIN } from '@config'
 import errorMiddleware from '@middlewares/error.middleware'
 import { stream } from '@utils/logger'
 import cookieParser from 'cookie-parser'
@@ -10,6 +10,7 @@ import express from 'express'
 import helmet from 'helmet'
 import hpp from 'hpp'
 import morgan from 'morgan'
+import mysql from 'mysql2'
 
 const app = express()
 
@@ -25,6 +26,12 @@ app.use(cookieParser())
 
 app.get('/', (req, res) => {
   res.json({ data: 'Hello world!' })
+  const connection = mysql.createConnection(DB_URL)
+
+  connection.query('show tables', function (err, results, fields) {
+    console.log({ results }) // results contains rows returned by server
+    console.log({ fields }) // fields contains extra metadata about results, if available
+  })
 })
 
 app.use(userRouter)
--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -4,6 +4,7 @@ const validateEnv = () => {
   cleanEnv(process.env, {
     NODE_ENV: str(),
     PORT: port(),
+    DB_URL: str(),
   })
 }
 
